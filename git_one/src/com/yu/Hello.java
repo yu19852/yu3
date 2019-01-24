31a6027bfba27e5827e4cf35459f67b5507565ab
yu <1985289262@qq.com>
2019-01-24 22:52:47 +0800

    修改Hello类中信息

--- git_one/src/com/yu/Hello.java
+++ git_one/src/com/yu/Hello.java
@@ -6,5 +6,7 @@ package com.yu;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hahahah");
+        System.out.println("牛B");
+        System.out.println("国会大厦");
     }
 }
